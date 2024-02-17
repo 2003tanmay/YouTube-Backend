8d4507881bc312b9c64d6dc66c7fa8fca6771b8f
Tanmay Pal <tanmayspal2003@gmail.com>
2024-02-17 18:07:04 +0530

    created delete method in cloudinary util

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -25,4 +25,19 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 }
 
-export {uploadOnCloudinary};
+const deleteFromCloudinary = async (public_id, resource_type = "image") => {
+    try {
+       if (!public_id) return null;
+ 
+       const response = await cloudinary.uploader.destroy(public_id, {
+          resource_type: `${resource_type}`,
+       });
+ 
+       console.log("file deleted from Cloudinary");
+       return response;
+    } catch (error) {
+       console.log("Failed while deleting the file from Cloudinary", error);
+    }
+ };
+ 
+export { uploadOnCloudinary, deleteFromCloudinary };
