d48cea40f10f789fa4147c474dc98209944c5426
Tanmay Pal <tanmayspal2003@gmail.com>
2024-02-21 12:37:01 +0530

    Added Like Functionality

--- src/models/like.model.js
+++ src/models/like.model.js
@@ -1,4 +1,5 @@
 import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const likeSchema = new Schema({
     video: {
@@ -19,5 +20,7 @@ const likeSchema = new Schema({
     },
     
 }, {timestamps: true})
+
+likeSchema.plugin(mongooseAggregatePaginate)
 
 export const Like = mongoose.model("Like", likeSchema)
