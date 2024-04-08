76fd97047ecc1cfb79d11d9b7624305c9790b3c3
Tanmay Pal <tanmayspal2003@gmail.com>
2024-04-08 20:52:33 +0530

    updated getCurrentUser controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -271,7 +271,9 @@ const getCurrentUser = asyncHandler(async (req, res) => {
     return res.status(200)
         .json(new ApiResponse(
             200,
-            req.user,
+            {
+                user: req.user,
+            },
             "current user fetched successfully"
         ))
 })
